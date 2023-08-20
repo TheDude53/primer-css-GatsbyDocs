bc2c2e0ff523787434e8b0efad03a27520e410d6
Chris Helder <46414358+TheDude53@users.noreply.github.com>
2023-08-20 05:16:17 -0500

    Adjust hard-linked "/css" for GitHub Pages
    It's a crappy solution, but it works. 'nuff said.

--- css/render-page.worker.js
+++ css/render-page.worker.js
@@ -77,7 +77,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/css/";
+/******/ 	__webpack_require__.p = "/primer-css-GatsbyDocs/css/";
 /******/
 /******/
 /******/ 	// Load entry module and return exports
